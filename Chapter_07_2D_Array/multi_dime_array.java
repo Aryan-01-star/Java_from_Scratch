7d8eae60cf203b26d365564446121c1d4e2142c2
Aryan-01-star <aryanpandey35247@gmail.com>
2026-08-31 16:11:11 +0530

    code

--- Chapter_07_2D_Array/multi_dime_array.java
+++ Chapter_07_2D_Array/multi_dime_array.java
@@ -13,8 +13,8 @@ public class multi_dime_array{
             }
         }
         System.out.println("The 2D array is:");
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < columns; j++) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
